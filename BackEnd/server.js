76e7b7eac696976ec37fb0ec43ba11733da73ce2
NotMohitMittal <notmohitmittal@gmail.com>
2026-06-02 21:04:33 +0530

    Fixing bugs

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,7 +8,6 @@ const connectDB = require("./src/DB/db");
 connectDB();
 
 if (process.env.ENVIRONMENT === "production") {
-<<<<<<< HEAD
   // Use ".." to step out of the BackEnd folder and into the FrontEnd folder
   const frontendPath = path.join(__dirname, "..", "FrontEnd", "dist");
   
@@ -16,24 +15,10 @@ if (process.env.ENVIRONMENT === "production") {
 
   app.get((req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
-=======
-  app.use(express.static(path.join(__dirname, "FrontEnd", "dist")));
-
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "FrontEnd", "dist", "index.html"));
->>>>>>> 12d0d7c8ef3817a4d8740256bccf0d7bdf1bb21c
   });
 }
-const PORT = process.env.PORT || 5000;
-
-<<<<<<< HEAD
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-=======
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
->>>>>>> 12d0d7c8ef3817a4d8740256bccf0d7bdf1bb21c
